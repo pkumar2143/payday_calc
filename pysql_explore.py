84eaa7bcb9d8f0fd8d42ac4d6d89668f9ea6cc1e
Pro Kumar <pkumar@udel.edu>
2025-08-29 23:07:02 -0400

    functionalizing monthly leftover

--- pysql_explore.py
+++ pysql_explore.py
@@ -2,32 +2,64 @@
 import sqlite3
 import calculator
 
-conn = sqlite3.connect('instance/payday.db')
-cursor = conn.cursor()
+def db_connect_and_grab():
+    conn = sqlite3.connect('instance/payday.db')
+    cursor = conn.cursor()
 
-# Test grabbing data & data selection
-sql_command = "SELECT * FROM expenses RIGHT JOIN user ON expenses.user_name = user.user_name"
+    # Test grabbing data & data selection
+    sql_command = "SELECT * FROM expenses RIGHT JOIN user ON expenses.user_name = user.user_name"
 
-cursor.execute(sql_command)
-rows = cursor.fetchall()
+    cursor.execute(sql_command)
+    rows = cursor.fetchall()
 
-# First row will need to have user data.
-user_data_row = rows[0]
-user_current_balance = user_data_row[7]
-print(f"User's current balance = {user_current_balance}, type={type(user_current_balance)}")
+    # First row will need to have user data.
+    user_data_row = rows[0]
+    #print(f"User Data Row = {user_data_row}")
+    user_current_balance = user_data_row[9] # Joining new user tables, shifts current balance idx.
+    print(f"User's current balance = {user_current_balance}, type={type(user_current_balance)}")
 
-# Collect monthly bills amounts
-monthly_bills = [0]*len(rows)                  # pre-allotting space
-for i in range(len(monthly_bills)):
-    monthly_bills[i] = rows[i][1]              # bill amts are index-1 elements of each row
-    print(f"Type = {type(monthly_bills[i])}")
+    # Collect monthly bills amounts
+    monthly_bills = [0]*len(rows)                  # pre-allotting space
+    for i in range(len(monthly_bills)):
+        monthly_bills[i] = rows[i][2]              # bill amts are index-2 elements of each row
+        #print(f"Type = {type(monthly_bills[i])}")
 
-print(f"Monthly bills = {type(monthly_bills)}")
+    #print(f"Monthly bills = {type(monthly_bills)}")
 
-monthly_leftover = calculator.leftover(current_balance=user_current_balance, 
-                                       expense_list=monthly_bills)
+    monthly_leftover = calculator.leftover(current_balance=user_current_balance, 
+                                           expense_list=monthly_bills)
 
-print(f"Amount leftover after all bills paid = {monthly_leftover}")
+    print(f"Amount leftover after all bills paid = {monthly_leftover}")
+    return monthly_leftover
+
+db_connect_and_grab()
+
+# conn = sqlite3.connect('instance/payday.db')
+# cursor = conn.cursor()
+
+# # Test grabbing data & data selection
+# sql_command = "SELECT * FROM expenses RIGHT JOIN user ON expenses.user_name = user.user_name"
+
+# cursor.execute(sql_command)
+# rows = cursor.fetchall()
+
+# # First row will need to have user data.
+# user_data_row = rows[0]
+# user_current_balance = user_data_row[7]
+# print(f"User's current balance = {user_current_balance}, type={type(user_current_balance)}")
+
+# # Collect monthly bills amounts
+# monthly_bills = [0]*len(rows)                  # pre-allotting space
+# for i in range(len(monthly_bills)):
+#     monthly_bills[i] = rows[i][1]              # bill amts are index-1 elements of each row
+#     print(f"Type = {type(monthly_bills[i])}")
+
+# print(f"Monthly bills = {type(monthly_bills)}")
+
+# monthly_leftover = calculator.leftover(current_balance=user_current_balance, 
+#                                        expense_list=monthly_bills)
+
+# print(f"Amount leftover after all bills paid = {monthly_leftover}")
 
 
 ###########################################
